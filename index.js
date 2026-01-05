e48f476eb6f344f27182471c39d57937bdd08095
lrodriguez05 <lazarorodriguezleyva@gmail.com>
2026-01-05 15:15:19 -0600

    set up Bun server in index.js and update package.json to use index.js as module

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 5555,
+  async fetch(req) {
+    return new Response("Hello, World!");
+  },
+});
+
+console.log(`El servidor esta funcionando en el puerto ${server.url}`);
